1282b0227af6bcd923aa0dca3469292b52456422
Gabriel Rodrigues <gabriel.rtlima@upe.br>
2020-12-19 19:40:02 -0300

    update in functions

--- system.py
+++ system.py
@@ -3,13 +3,14 @@ import json
 import sys
 import os
 
-def reiniciar_programa():
-    os.execv(sys.executable, ['python'] + sys.argv) 
-
 #CONEXÃO COM O BANCO DE DADOS
 client = pymongo.MongoClient("mongodb+srv://loginsystem:123@db.jkcwq.mongodb.net/<users>?retryWrites=true&w=majority")
 db = client.users
 collection = db["users"]
+
+#FUNÇÃO PARA REINICIAR O PROGRAMA
+def reiniciar_programa():
+    os.execv(sys.executable, ['python'] + sys.argv) 
 
 #FUNÇÃO DE CADASTRO NO BANCO DE DADOS
 def cadastrar_db():
@@ -18,8 +19,25 @@ def cadastrar_db():
     "Usuario":usuario,
     "SENHA":senha,
     "idade":idade}
-    id = db.users.insert_one(document).inserted_id
-    print(id)
+    db.users.insert_one(document).inserted_id
+    print('Usuario cadastrado.')
+
+    while True:
+        final_cadastro = input('DIGITE 1 SE DESEJA REALIZAR LOGIN | DIGITA 2 SE DESEJA SAIR\n')
+
+        if final_cadastro == '1':
+            fazer_login()
+            break
+
+        elif final_cadastro == '2':
+            quit()
+
+        else:
+            print('VOCÊ INSERIU O NÚMERO ERRADO, INSIRA NOVAMENTE.')
+        
+#FUNÇÃO PARA FAZER LOGIN
+def fazer_login():
+    print('fazer login')
 
 #FUNÇÃO DE CADASTRO DE DADOS
 def cadastrar_dados():
@@ -29,7 +47,7 @@ def cadastrar_dados():
     global senha
     global idade
     nome = input('Digite seu nome: ')
-    email = input('Digite seu e-mail: ')
+    email = input('Digite seu e-mail(OPCIONAL): ')
     usuario = input('Digite seu nome de usuário: ')
     verificar_usuario()
     senha = input('Digite sua senha: ')
@@ -55,10 +73,14 @@ def verificar_usuario():
                 reiniciar_programa()
             else:
                 quit()
-
-inicio = input("Bem vindo ao sistema de login v1.0\nDigite 1 se deseja criar uma nova conta | Digite 2 se deseja realizar login\n")
+print('-'*80)
+print('|' + ' '*22 + 'Bem vindo ao sistema de login v1.0'+' '* 22 + '|')
+print('-'*80)
+inicio = input("Digite 1 se deseja criar uma nova conta | Digite 2 se deseja realizar login\n")
 
 if inicio == '1':
     cadastrar_dados()
     cadastrar_db()
 
+elif inicio == '2':
+    fazer_login()
